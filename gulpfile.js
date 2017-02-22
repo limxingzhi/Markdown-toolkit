d838b5dec73023939433e100dfd296eca3d49bd6
limxingzhi <coldsandstorm97@gmail.com>
2017-02-22 23:59:33 +0800

    Changed the index to readme
    - renamed the previous index.md to readme.md
    - updated middleware at gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var sass = require('gulp-sass');
 var pathMD = ['./docs/*.md', './README.md'];
 var pathBUILD = ['./build/*'];
 
+var middlewareExclude = ['.html', '.css', '.js'];
+
 gulp.task('build:sass', function () {
   return gulp.src('./styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -34,7 +36,22 @@ gulp.task('clean', function () {
 gulp.task('serve', function () {
   browserSync.init({
     server: {
-      baseDir: "./build"
+      baseDir: "./build",
+      index: "readme.html"
+    },
+    middleware: function(req, res, next){
+      var containsExtension = false;
+
+      containsExtension = middlewareExclude.some(excludeExtension => {
+        if (req.url.includes(excludeExtension)) {
+          return true;
+        }
+      });
+
+      containsExtension = containsExtension || req.url === '' || req.url === '/';
+
+      req.url += containsExtension ? '' : '.html';
+      return next();
     },
     port: 8080,
     ui: {
